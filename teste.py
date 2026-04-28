be830b6cc5554c87a2957dedb8804733d7f0e5e5
luizabertoldoseda-hash <luizabertoldoseda@gmail.com>
2026-04-28 15:41:25 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,4 +2,9 @@ import streamlit as st
 
 st.title('Luiza S')
 st.write("Esse é o meu texto")
+
+nome = st.text_input('Digite o seu nome')
+if nome:
+    st.write(nome, 'é um cara legal!')
+  
 st.image("https://images.cults3d.com/Cysw2zADhd70OdIduOOQtXqfSyA=/516x516/filters:no_upscale():format(webp)/https://fbi.cults3d.com/uploaders/38920753/illustration-file/626491b5-8223-499a-869a-08b114c95cfb/tung-tung.jpg", caption="Legenda da Imagem", use_container_width=True)
