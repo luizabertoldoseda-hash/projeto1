be84ba4dba5d46efa2ba60bb81e40a58ca1a0b94
luizabertoldoseda-hash <luizabertoldoseda@gmail.com>
2026-04-28 15:15:02 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('Luiza S')
 st.write("Esse é o meu texto")
-st.image("https://cults3d.com/pt/modelo-3d/arte/tung-tung-tung-sahur-anidala00?srsltid=AfmBOor8BDLq9-sLxRnuXkM83KWkiAkDEJwqmQ0xgc_Z-xP3fCtYUjXU", caption="Legenda da Imagem", use_container_width=True)
+st.image("https://cults3d.com/pt/modelo-3d/arte/tung-tung-tung-sahur-anidala00?srsltid=AfmBOor8BDLq9-sLxRnuXkM83KWkiAkDEJwqmQ0xgc_Z-xP3fCtYUjXU"), caption="Legenda da Imagem", use_container_width=True)
