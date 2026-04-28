0ff121a57dfa82806d421300120a295882719646
luizabertoldoseda-hash <luizabertoldoseda@gmail.com>
2026-04-28 15:17:50 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('Luiza S')
 st.write("Esse é o meu texto")
-st.image("https://cults3d.com/pt/modelo-3d/arte/tung-tung-tung-sahur-anidala00?srsltid=AfmBOor8BDLq9-sLxRnuXkM83KWkiAkDEJwqmQ0xgc_Z-xP3fCtYUjXU"), caption="Legenda da Imagem", use_container_width=True)
+st.image("https://images.cults3d.com/Cysw2zADhd70OdIduOOQtXqfSyA=/516x516/filters:no_upscale():format(webp)/https://fbi.cults3d.com/uploaders/38920753/illustration-file/626491b5-8223-499a-869a-08b114c95cfb/tung-tung.jpg"), caption="Legenda da Imagem", use_container_width=True)
