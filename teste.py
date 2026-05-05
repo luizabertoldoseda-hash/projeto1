c4cfb16aaf34e6efbf279fe1e863b7f756b6e05b
luizabertoldoseda-hash <luizabertoldoseda@gmail.com>
2026-05-05 14:21:44 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -5,6 +5,7 @@ st.write("Esse é o meu texto")
 
 nome = st.text_input('Digite o seu nome')
 if nome:
-    st.write(nome, 'é um cara legal!')
+    st.write(nome,'loves tung tung tung sahur!')
   
 st.image("https://images.cults3d.com/Cysw2zADhd70OdIduOOQtXqfSyA=/516x516/filters:no_upscale():format(webp)/https://fbi.cults3d.com/uploaders/38920753/illustration-file/626491b5-8223-499a-869a-08b114c95cfb/tung-tung.jpg", caption="Legenda da Imagem", use_container_width=True)
+st.video("https://www.tiktok.com/@chickennoodle266/video/7625090314801401119")
