9e8060f8a2418d47be3dfc8cc18785e0a8850581
luizabertoldoseda-hash <luizabertoldoseda@gmail.com>
2026-05-05 15:06:03 -0300

    Update deputados.py

--- deputados.py
+++ deputados.py
@@ -3,3 +3,14 @@ import pandas as pd
 
 df = pd.read_csv('deputados_2022.csv')
 st.dataframe(df)
+
+sigla = st.text_input('Digite a sigla de algum partido')
+
+if sigla 
+    filtrado = df[df["sigla"].str.lower() == sigla.lower()]
+
+    if not filtrado.empty:
+        st.write(f"Deputados do {sigla}:")
+        st.dataframe(filtrado)
+    else:
+        st.write("Nenhum deputado encontrado desse partido.")
