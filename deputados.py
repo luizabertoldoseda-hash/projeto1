21cb43b6f6a93ab715797d8c0505f4cf6f009a8d
luizabertoldoseda-hash <luizabertoldoseda@gmail.com>
2026-05-05 15:32:36 -0300

    Update deputados.py

--- deputados.py
+++ deputados.py
@@ -14,3 +14,21 @@ if sigla:
         st.dataframe(filtrado)
     else:
         st.write("Nenhum deputado encontrado desse partido.")
+ 
+
+if "sexo" in filtrado.columns:
+            filtrado = filtrado.copy()
+            filtrado["sexo"] = filtrado["sexo"].str.upper()
+
+            contagem = filtrado["sexo"].value_counts()
+
+          
+            contagem.index = contagem.index.map({"M": "Homens","F": "Mulheres"})
+
+            st.write("Distribuição por gênero:")
+            st.bar_chart(contagem)
+    else:
+            st.write("Coluna 'sexo' não encontrada no dataset.")
+
+    else:
+        st.write("Nenhum deputado encontrado desse partido.")
